9bf687c9b2f945f4c03679f958bc30a39891cb51
Sigurd K. Würtz <201906361@post.au.dk>
2024-02-21 15:07:05 +0100

    prompt engenering

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+public class ExtractedData
+{
+    public string Label { get; set; }
+    public string Orientation { get; set; }
+    public string TextContent { get; set; }
+}
+
+public static class Utility
+{
+    public static ExtractedData extractDataFromResponse(string response)
+    {
+        string textcontent = response.Split("}")[1].TrimStart('.').TrimStart();
+
+        char[] separators = new char[] { '{', '}', ',' };
+        string[] responseAsSubstrings = response.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        
+        return new ExtractedData
+        {
+            Label = responseAsSubstrings?[0],
+            Orientation = responseAsSubstrings?[1],
+            TextContent = textcontent
+        };
+    }
+}
